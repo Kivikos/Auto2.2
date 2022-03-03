57caee515a85487a893590a569b07f045c67b69a
Konstantin Voytenko <koc.voytenko@gmail.com>
2022-03-03 18:56:28 +0300

    Code edited

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -3,18 +3,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    private WebDriver driver;
+    int days = 7;
 
     @BeforeAll
     public static void SetUpCLass() {
@@ -26,39 +24,58 @@ public class CardDeliveryTest {
         open("http://localhost:9999/");
     }
 
-    public String delivery(boolean date) {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DATE, 7);
-        if (date) {
-            return new SimpleDateFormat("d").format((calendar.getTime()));
-        } else {
-            return new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime());
-        }
+    String getFormattedDate(int days, String patternOfDate) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(patternOfDate));
+    }
+
+    int yearOffset(int days) {
+        return LocalDate.now().plusDays(days).getYear() - LocalDate.now().plusDays(3).getYear();
+    }
+
+    int monthOffset(int days) {
+        return LocalDate.now().plusDays(days).getMonthValue() - LocalDate.now().plusDays(3).getMonthValue();
     }
 
     @Test
     void shouldSendForm() {
         $("[data-test-id=city] .input__control").setValue("Нижний Новгород");
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL + "A"), Keys.BACK_SPACE, delivery(false));
+        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL + "A"), Keys.BACK_SPACE, LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu")));
         $("[data-test-id=name] [name=name]").setValue("Войтенко Константин");
         $("[data-test-id=phone] [name=phone]").setValue("+79991236558");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + delivery(false)));
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"))));
         $(".icon_name_close").click();
     }
 
     @Test
     void shouldSendFormAnotherVar() {
-        $("[data-test-id=city] .input__control").setValue("Ниж");
+        $("[data-test-id=city] .input__control").setValue("Ни");
         $$(".menu-item__control").findBy(text("Нижний Новгород")).click();
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL + "A"), Keys.DELETE);
-        $$(".calendar__day").findBy(text(delivery(true))).click();
+        $(".icon-button__text>.icon_name_calendar").click();
+        if (yearOffset(days) > 0) {
+            for (int i = 0; i < yearOffset(days); i++) {
+                $(".calendar__arrow_direction_right[data-step='12']").click();
+            }
+            if (monthOffset(days) > 0) {
+                for (int i = 0; i < monthOffset(days); i++) {
+                    $(".calendar__arrow_direction_right[data-step='1']").click();
+                }
+            } else {
+                for (int i = 0; i > monthOffset(days); i--) {
+                    $(".calendar__arrow_direction_left[data-step='-1']").click();
+                }
+            }
+        } else {
+            for (int i = 0; i < monthOffset(days); i++) {
+                $(".calendar__arrow_direction_right[data-step='1']").click();
+            }
+        }
+        $$(".calendar__day").findBy(text(LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("d")))).click();
         $("[data-test-id=name] [name=name]").setValue("Войтенко Константин");
-        $("[data-test-id=phone] [name=phone]").setValue("+79865412233");
+        $("[data-test-id=phone] [name=phone]").setValue("+79991236558");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + delivery(false)));
-        $(".icon_name_close").click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + getFormattedDate(days, "dd.MM.yyyy")));
     }
 }
